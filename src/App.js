e70f7c9d5709b7b11f5dc0091766a50cec01477e
daiki-tadokoro <81633632+daiki-tadokoro@users.noreply.github.com>
2022-01-03 08:24:21 +0900

    fix:add-Organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SerchInput } from "./components/molecules/SerchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "はな",
+  image: "https://source.unsplash.com/Sg3XwuEpybU",
+  email: "11111@aa.com",
+  phone: "000-3344-5566",
+  company: { name: "ああああああ株式会社" },
+  website: "https://www.google.com/?hl=ja"
+};
 
 export default function App() {
   return (
@@ -11,6 +21,8 @@ export default function App() {
       <SecondaryButton>テスト2</SecondaryButton>
       <br />
       <SerchInput />
+      <br />
+      <UserCard user={user} />
     </div>
   );
 }
